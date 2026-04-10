1692b026956380bc612455d4e349519b9b5bce32
Kunwar Abhishek Singh <ab251202@gmail.com>
2026-04-10 16:01:43 +0530

    fixing swagger

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const { login } = require("../controllers/authController");
 
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags:
+ *       - Auth
+ *     responses:
+ *       200:
+ *         description: Login successful
+ */
 router.post("/login", login);
 
 module.exports = router;
